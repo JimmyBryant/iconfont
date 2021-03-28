648db696989a61f0c925abd5c5069bd1aaefe561
bitterg <lovekugua@gmail.com>
2021-03-28 19:37:52 +0800

    add tag img

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,58 +1,4 @@
-// $(function() {
-//       $('main').click(function() {
-//            $(".playOnphone .mark").hide();
-//            // event.stopPropagation();
-//        });
 
-//       $('.playOnphone .mark').click(function() {
-//            $(this).show();
-//            event.stopPropagation();
-//        });
-
-//        $('.playOnphone .playIt')
-//            .css("cursor", "pointer")
-//            .click(function() {
-//                $(".playOnphone .mark").toggle();
-//                event.stopPropagation();
-//            });
-//    });
-
-
-//  var Lhref = window.location.href;
-
-//  $("#like").click(function(){
-//    var localhref = localStorage.getItem(Lhref);
-//      $(this).toggleClass("remove_like").toggleClass("click_like");
-//      if (localhref) {
-// localStorage.removeItem(Lhref);
-//      }else{
-//        localStorage.setItem(Lhref, "like");
-//      }
-//  })
-
-//  jQuery(document).ready(function() {
-//    var localhref = localStorage.getItem(Lhref);
-//      if (localhref) {
-// $("#like").addClass("remove_like").removeClass("click_like");
-//      }else{
-// $("#like").removeClass("remove_like").addClass("click_like");
-//      }
-//  })
-
-// function ClickAjax(){
-//  var href = window.location.href;
-//  var params={"game":"among-them-bubble-shooter","ref":href};
-//        $.ajax({
-//            type: "POST",
-//            url: "/support/clicks",
-//            dataType: "json",
-//            crossDomain: true,
-//            data: params,
-//            success: function(result){
-//                    console.log(result);
-//                }
-//            });
-//    }
 function setCookie(name, value, exdays) {
     var date = new Date();
     date.setDate(date.getDate() + exdays);
@@ -94,56 +40,65 @@ window.onload = function () {
 if (document.body.clientWidth <= 767) {
     $("#share").click(function () {
         navigator.share({
-            title: 'gamejoystick',
+            title: 'weGame',
             text: '',
             url: location.href + '/?utm_source=game&utm_medium=share',
         })
-            .then(() => console.log('Share was successful.'))
-            .catch((error) => console.log('Sharing failed', error));
+        .then(() => console.log('Share was successful.'))
+        .catch((error) => console.log('Sharing failed', error));
 
     })
 }
 /**
- * 添加到主屏幕
+ * 添加PWA到浏览器
  */
 let deferredPrompt;
-const addBtn = document.querySelectorAll('.home_add');
-const footerBtn = document.querySelector('#home');
-footerBtn.style.display = 'none';
+const addBtn = $('.home_add');
+const footerBtn = $('#home');
+footerBtn.css('display','none');
 var i;
 
 //浏览器触发Add to home 时会触发一个beforeinstallprompt事件，我们监听这个事件
 window.addEventListener('beforeinstallprompt', (e) => {
-    console.log(e.platforms);
+
     e.preventDefault(); // 阻止浏览器的默认事件
     deferredPrompt = e; // 储存事件对象，方便在之后的按钮事件中手动触发
 
     $(window).scroll(function (event) {
         var winPos = $(window).scrollTop();
         if (winPos > $(window).height() && document.body.clientWidth <= 1024) {
-            footerBtn.style.display = 'flex';
+            footerBtn.css('display','flex');
         }
     });
-    for (i = 0; i < addBtn.length; i++) {
-        addBtn[i].addEventListener('click', (e) => {
 
-            deferredPrompt.prompt(); // 弹出Add to home 提示
-            // addBtn.style.display = 'none'; // 用户点击后将按钮隐藏
-            // 等待用户操作结果
-            deferredPrompt.userChoice.then((choiceResult) => {
-                if (choiceResult.outcome === 'accepted') {
-                    console.log('User accepted the Add to home prompt');
-                }
-                else {
-                    console.log('User dismissed the Add to home prompt');
-                }
-                // 只能用一次，不管用户拒绝还是同意都不在弹出
-                // deferredPrompt = null;
-            });
+    addBtn.on('click', (e) => {
+
+        deferredPrompt.prompt(); // 弹出Add to home 提示
+        // addBtn.style.display = 'none'; // 用户点击后将按钮隐藏
+        // 等待用户操作结果
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the Add to home prompt');
+            }
+            else {
+                console.log('User dismissed the Add to home prompt');
+            }
+            // 只能用一次，不管用户拒绝还是同意都不在弹出
+            // deferredPrompt = null;
         });
-    }
+    });
+    
 });
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then(() => { console.log('Service Worker Registered'); });
+}
+
+/**
+ * do somethings when document ready 
+ */
 $(function () {
     // lazyload images
     lazyload();
@@ -191,7 +146,7 @@ $(function () {
 
         // 游戏页
         insertGads("play_ad_01", "ca-pub-2994572689024438", "7317190342");
-        insertGads("play_ad_02", "ca-pub-2994572689024438", "6004108672", false);
+        insertGads("play_ad_02", "ca-pub-2994572689024438", "6004108672");
         insertGads("play_ad_03", "ca-pub-2994572689024438", "9175947364");
     }
 
@@ -220,14 +175,14 @@ $(function () {
 
         var localmark = localStorage.getItem("localmark");
         arr = localmark.substring(0, localmark.length - 1).split("/");
-        console.log(arr);
+        // console.log(arr);
         $(arr).each(function (i, item) {
             // console.log(arr);
             if (arr[i] == deletext) {
                 arr.splice(jQuery.inArray(arr[i], arr), 1);
             }
         })
-        console.log(arr);
+        // console.log(arr);
         if (arr.length != 0) {
             arr = arr.join('/');
             arr = arr + '/';
@@ -327,7 +282,7 @@ $(function () {
             // alert(arr.length);
             $(".fav_num").text(arr.length);
             var localjson1 = localStorage.getItem(arr[i]);
-            console.log(localjson1);
+            // console.log(localjson1);
             var localjson1 = JSON.parse(localjson1);
             str1 = '<li class=\"pop_li\"><div class=\"pop_li_box\">';
             str1 += '<a href="' + localjson1.href2 + '" class=\"game_img\">';
@@ -374,7 +329,7 @@ $(function () {
         } else {
             var txt2 = $(".game_title").text();
             var href2 = location.href;
-            var img2 = $(".gamejoy_img").attr('src');
+            var img2 = $('.img_topfix>img').attr('src') || $(".gamejoy_img").attr('src');
             // var playNum2 = "14K Plays";
 
             var localjson = {
